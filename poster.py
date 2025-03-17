53e96f2e5dcfefff97ca5a180c97884535787ca2
/ Lynn ?? <sampalynn@gmail.com>
2025-03-18 00:42:59 +0700

    poster.py

--- poster.py
+++ poster.py
@@ -6,7 +6,7 @@ try:
   
   chat_id = int(sys.argv[1])
   file = str(chat_id)+'.png'
-  bot = telebot.TeleBot("SET TOKEN")
+  bot = telebot.TeleBot("8193457229:AAGH8k1NRdobNGzndOQyA_hJ1hFDhqfI-Aw")
   img = open(file, 'rb')
   bot.send_photo(chat_id, img)
   os.remove(file)
